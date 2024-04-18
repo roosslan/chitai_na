53db4b700a641a9679ee178d9d3b6531087b9832
unknown <D:\Programs\theBat>
2024-04-19 00:16:41 +0300

    sorted and separated

--- Version.h
+++ Version.h
@@ -1,11 +1,11 @@
-#define FILE_VERSIONSTRING     "1.1.0.0\0"
+#define FILE_VERSIONSTRING     "1.2.0.0\0"
 #define FILE_MAJOR             1
 #define FILE_MINOR             1
 #define FILE_BUILD             0
 #define FILE_REVISION          0
 
-#define COMPANY_NAME      "www.0x9.io\0"
-#define LEGAL_COPYRIGHT   "Copyright (C) 2023 www.0x9.io\0"
+#define COMPANY_NAME      "www.0x1.dev\0"
+#define LEGAL_COPYRIGHT   "Copyright (C) 2020-2024 www.0x1e.dev\0"
 #define LEGAL_TRADEMARKS  "\0"
 #define FILE_DESCRIPTION  "chitai na\0"
 #define INTERNAL_NAME     "chitai na\0"
