b298427f8bcdcce68f4e65702418b1b0bc67f735
rooslan <open.ok@gmail.com>
2026-05-25 21:26:14 +0300

    md:pict added

--- Version.h
+++ Version.h
@@ -1,11 +1,11 @@
-#define FILE_VERSIONSTRING     "1.2.0.0\0"
+#define FILE_VERSIONSTRING     "1.2.5.0\0"
 #define FILE_MAJOR             1
 #define FILE_MINOR             1
 #define FILE_BUILD             0
 #define FILE_REVISION          0
 
 #define COMPANY_NAME      "a_g0@mail.ru\0"
-#define LEGAL_COPYRIGHT   "Copyright (C) 2020-2025 a_g0@mail.ru\0"
+#define LEGAL_COPYRIGHT   "Copyright (C) 2019-2026 a_g0@mail.ru\0"
 #define LEGAL_TRADEMARKS  "\0"
 #define FILE_DESCRIPTION  "chitai na\0"
 #define INTERNAL_NAME     "chitai na\0"
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Please make sure the option "Media features" is enabled (using classic "Programs and Features" control panel), especially "Windows Media Player" option, as chitai_na looking for C:\Windows\System32\wmp.dll
+ \
+![chitai_na.gif](https://github.com/roosslan/chitai_na/blob/trunk/chitai_na.gif?raw=true)
